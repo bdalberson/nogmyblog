9b1114cca93c341be894bc94ce3b563db67cfb62
bdalberson <Brian.alberson@gmail.com>
2023-05-05 15:04:23 -0700

    server.js finished

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
 const path = require('path');
 const express = require('express');
-const sessions = require('express-session');
+const session = require('express-session');
 const exphbs = require('express-handlebars');
-const route = ('./controllers');
+const routes = ('./controllers');
 const sequelize = require('./config/connection');
 const sequelizeSession = require('connect-session-sequelize')(sessions.Store);
 
-const app = express()
+const app = express();
+const port = process.env.PORT || 3001;
+
+const  sess =
+{
+    secret: process.env.SECRET, 
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new sequelizeSession({db:sequelize})
+};
+
+app.use(session(sess));
+
+app.use(express.json());
+app.use(express.urlencoded({extend:true}));
+app.use(routes);
+sequelize.sync().then(()=> {
+
+    app.listen(port, ()=> console.log(`listening on port ${port}`))
+
+})
 
 
 
